b3b9a1d5de1320b9bea7edbfb49d30fcf4b375e9
cdaringe <christopher.dieringer@gmail.com>
2018-07-03 15:44:51 -0700

    chore(dev): patch editor settings

--- go_broke/data_formatter.py
+++ go_broke/data_formatter.py
@@ -6,13 +6,13 @@ def format_attributes(data, window, days_in_future):
     slice_start = 0
     slice_stop = window
 
-    input = np.ndarray(shape=(len(data) - days_in_future - window, window))
-    label = np.ndarray(shape=(len(data) - days_in_future - window, 1))
+    prices = np.ndarray(shape=(len(data) - days_in_future - window, window))
+    future_prices = np.ndarray(shape=(len(data) - days_in_future - window, 1))
 
     while slice_stop + days_in_future < len(data):
-        label[slice_start] = data[slice_stop + days_in_future]
-        input[slice_start][:] = data[slice_start:slice_stop]
+        future_prices[slice_start] = data[slice_stop + days_in_future]
+        prices[slice_start][:] = data[slice_start:slice_stop]
         slice_start += 1
         slice_stop += 1
 
-    return input, label
+    return prices, future_prices
--- go_broke/main.py
+++ go_broke/main.py
@@ -3,7 +3,7 @@ import data_formatter
 from datetime import datetime
 import time
 import logging
-import bollinger as bol
+# import bollinger as bol
 logging.basicConfig()
 
 LOGGER = logging.getLogger(__file__)
@@ -16,12 +16,14 @@ def main():
     #     aq.fetch_df(symbol, datetime.utcnow(), exchange='NYSE')
     #     time.sleep(0.1)
     df = aq.fetch_df('SPY', exchange='NYSEARCA')
-    input, label = data_formatter.format_attributes(df['Close'], 30, 3)
-    # Split data into training and test set. Allow for the possilbity that the test set is 0 length
+    prices, future_price = data_formatter.format_attributes(df['Close'], 30, 3)
+
+    nueral_net(input, label)
+    # Split data into training and test set. Allow for the possibility that the test set is 0 length
     # Normilize the dats
     # Do ML
     #df = df.rename(columns={'Close':'SPY'})
-    bol.bollinger(df)
+    # bol.bollinger(df)
 
 
 if __name__ == '__main__':
